94dfb2b92598b1310b74fb0ab7a90a9637c42a87
neshdev999 <71468718+neshdev999@users.noreply.github.com>
2021-01-20 16:43:49 -0500

    commit-2-more-features
    favicon,placeholder header and footer

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 
-
+function generateImage(responseJson){
+    $(".userOuputHolder").append(`<div class="dogImageContainer"><img class="dogImageStyle" src='${responseJson.message}' alt='dog'></div>`);
+}
 
 
 function getDogImage() {
     fetch('https://dog.ceo/api/breeds/image/random')
       .then(response => response.json())
-      .then(responseJson => console.log(responseJson))
+      .then(
+          responseJson => {
+            generateImage(responseJson);
+            console.log(responseJson);  
+          }        
+      )
       .catch(error => alert('Something went wrong. Try again later.'));
 }
 
@@ -13,6 +20,14 @@ function getDogImage() {
 function watchForm() {
     $('form').submit(event => {
       event.preventDefault();
+      // Remove placeholder header
+      if($('.placeHolderHeader').length){
+        $(".placeHolderHeader").remove();
+      }
+      
+      if($('.dogImageContainer').length){
+        $( ".dogImageContainer" ).remove();
+    }
       var dogCount = $('#numberOfDogIndicator').val();
       console.log(dogCount);
       for(let i=0; i<dogCount; i++){
@@ -22,8 +37,57 @@ function watchForm() {
 }
 
 
+/* Footer */
+function generateFooter() {
+    const yogiFooterBase = yogiFooter();
+    $('#footer').append(yogiFooterBase);
+}
+
+function yogiFooter() {
+    return `<div class="footContain"><div class="footStyles"><span>&nbsp;Dogs Panel&nbsp;&nbsp;<br></span><span>Nesh &copy; ${getCopyRightYear()}</span></div></div>`;
+}
+
+function getCopyRightYear() {
+    return new Date().getFullYear();
+}
+
+$(window).bind("load", function() { 
+       
+    var footerHeight = 0,
+        footerTop = 0,
+        $footer = $("#footer");
+        
+    positionFooter();
+    
+    function positionFooter() {
+    
+             footerHeight = $footer.height();
+             footerTop = ($(window).scrollTop()+$(window).height()-footerHeight)+"px";
+    
+            if ( ($(document.body).height()+footerHeight) < $(window).height()) {
+                $footer.css({
+                     position: "absolute"
+                }).animate({
+                     top: footerTop
+                })
+            } else {
+                $footer.css({
+                     position: "static"
+                })
+            }
+            
+    }
+
+    $(window)
+            .scroll(positionFooter)
+            .resize(positionFooter)
+            
+});
+
+
 /* Initialize App */
 $(function() {
     console.log('App loaded! Waiting for submit!');
     watchForm();
+    generateFooter();
 });
