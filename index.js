3eeb789541a778996d8368360d21718de4b154ee
neshdev999 <71468718+neshdev999@users.noreply.github.com>
2021-01-24 14:24:15 -0500

    commit-7-code-cleanup
    code cleanup, aria-handler, styles improvement

--- index.js
+++ index.js
@@ -1,64 +1,82 @@
 /* Generates Images after form submission */
-function generateImage(responseJson){
+function generateImage(responseJson) {
     $(".userOuputHolder").append(`<div class="dogImageContainer"><img class="dogImageStyle" src='${responseJson.message}' alt='dog'></div>`);
 }
 
 /* Generates Images on default page load */
-function generateDefaultImages(responseJson){
+function generateDefaultImages(responseJson) {
     $(".placeHolderImageHeaderContainer").append(`<div class="dogImageContainer"><img class="dogImageStyle" src='${responseJson.message}' alt='dog'></div>`);
 }
 
 /* Dog Api call */
 function getDogImage(defaultValAvailCheck) {
     fetch('https://dog.ceo/api/breeds/image/random')
-      .then(response => response.json())
-      .then(
-          responseJson => {
-              if(defaultValAvailCheck === true){
-                generateDefaultImages(responseJson);
-              }else if(defaultValAvailCheck === false){
-                generateImage(responseJson);
-              }
+        .then(response => response.json())
+        .then(
+            responseJson => {
+                if (defaultValAvailCheck === true) {
+                    generateDefaultImages(responseJson);
+                } else if (defaultValAvailCheck === false) {
+                    generateImage(responseJson);
+                }
 
-            console.log(responseJson);  
-          }        
-      )
-      .catch(error => alert('Something went wrong. Try again later.'));
+                console.log(responseJson);
+            }
+        )
+        .catch(error => alert('Something went wrong. Try again later.'));
 }
 
 /* Handle actions after form submission */
+
+
+function handleEnterClicks() {
+
+    $("#numberOfDogIndicator").keyup(function(event) {
+        if (event.keyCode === 13) {
+            $("#dog-select").attr('aria-pressed', true)
+            const pressedBool = $('#dog-select').attr('aria-pressed') === 'true';
+            console.log(pressedBool);
+            if (pressedBool) {
+                $('#submitButtonId').click();
+            }
+        }
+    });
+}
+
 function watchForm() {
     $('form').submit(event => {
-      event.preventDefault();
-      // $('.userOuputHolder').addClass('hidden');
-      $('.userOutputDefaultHolder').addClass('hidden');
-      // Remove placeholder header
-      if($('.placeHolderHeader').length){
-        $(".placeHolderHeader").remove();
-      }
-      
-      if($('.dogImageContainer').length){
-        $( ".dogImageContainer" ).remove();
-    }
-      var dogCount = $('#numberOfDogIndicator').val();
-      console.log(dogCount);
-      for(let i=0; i<dogCount; i++){
-        getDogImage(false);
-      }
-      $('.userOuputHolder').removeClass('hidden');
+        event.preventDefault();
+        // Make default view hidden
+        $('.userOutputDefaultHolder').addClass('hidden');
+        // Remove placeholder header
+        if ($('.placeHolderHeader').length) {
+            $(".placeHolderHeader").remove();
+        }
+
+        if ($('.dogImageContainer').length) {
+            $(".dogImageContainer").remove();
+        }
+        var dogCount = $('#numberOfDogIndicator').val();
+        console.log(dogCount);
+        for (let i = 0; i < dogCount; i++) {
+            getDogImage(false);
+        }
+        $('.userOuputHolder').removeClass('hidden');
     });
 }
 
 /* Load three random dog images on page load */
-function initialDefaultDogImages(){
-  // set input number to default value
+function initialDefaultDogImages() {
+    // set input number to default value
     $('#numberOfDogIndicator').val(3);
-    for(let i=0; i<3; i++){
+    for (let i = 0; i < 3; i++) {
         getDogImage(true);
-      }
+    }
     $('.placeHolderImageHeaderContainer').removeClass('hidden');
     $('.userOuputHolder').removeClass('hidden');
 }
+
+
 
 
 /* Footer */
@@ -75,37 +93,35 @@ function getCopyRightYear() {
     return new Date().getFullYear();
 }
 
-$(window).bind("load", function() { 
-       
+$(window).bind("load", function() {
+
     var footerHeight = 0,
         footerTop = 0,
         $footer = $("#footer");
-        
-    positionFooter();
-    
-    function positionFooter() {
-    
-             footerHeight = $footer.height();
-             footerTop = ($(window).scrollTop()+$(window).height()-footerHeight)+"px";
-    
-            if ( ($(document.body).height()+footerHeight) < $(window).height()) {
-                $footer.css({
-                     position: "absolute"
-                }).animate({
-                     top: footerTop
-                })
-            } else {
-                $footer.css({
-                     position: "static"
-                })
-            }
-            
-    }
 
+    positionFooter();
+
+    function positionFooter() {
+
+        footerHeight = $footer.height();
+        footerTop = ($(window).scrollTop() + $(window).height() - footerHeight) + "px";
+
+        if (($(document.body).height() + footerHeight) < $(window).height()) {
+            $footer.css({
+                position: "absolute"
+            }).animate({
+                top: footerTop
+            })
+        } else {
+            $footer.css({
+                position: "static"
+            })
+        }
+    }
     $(window)
-            .scroll(positionFooter)
-            .resize(positionFooter)
-            
+        .scroll(positionFooter)
+        .resize(positionFooter)
+
 });
 
 
@@ -113,6 +129,7 @@ $(window).bind("load", function() {
 $(function() {
     console.log('App loaded! Waiting for submit!');
     initialDefaultDogImages();
+    handleEnterClicks();
     watchForm();
     generateFooter();
 });
